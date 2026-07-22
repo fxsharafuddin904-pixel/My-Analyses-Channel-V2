a683fe529f8614433e7985a7a1ef6ec0b8fb93f4
fxsharafuddin904-pixel <fxsharafuddin904@gmail.com>
2026-07-22 15:57:16 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// index.js
+const { Telegraf } = require("telegraf");
+const config = require("./config");
+
+const { registerStart } = require("./start");
+const { registerRequest } = require("./request");
+const { registerChannel } = require("./channel");
+const { registerNotification } = require("./notification");
+const { registerAdmin } = require("./admin");
+
+const bot = new Telegraf(config.BOT_TOKEN);
+
+/* =========================================
+   REGISTER MODULES
+========================================= */
+
+registerStart(bot);
+
+registerRequest(bot);
+
+registerChannel(bot);
+
+registerNotification(bot);
+
+registerAdmin(bot);
+
+/* =========================================
+   BOT START
+========================================= */
+
+bot.launch(() => {
+
+    console.log("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
+    console.log("🤖 Trading Request Bot");
+    console.log("✅ Bot Online");
+    console.log("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
+
+});
+
+/* =========================================
+   STOP BOT
+========================================= */
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+
+process.once("SIGTERM", () => bot.stop("SIGTERM"));

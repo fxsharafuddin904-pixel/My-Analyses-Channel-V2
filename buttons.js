c04a9529daa85a481a96b0b20f7b907044c6e966
fxsharafuddin904-pixel <fxsharafuddin904@gmail.com>
2026-07-22 15:52:01 +0600

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -1,1 +1,76 @@
-// buttons.js
+const { Markup } = require("telegraf");
+
+function startButtons() {
+
+    return Markup.inlineKeyboard([
+
+        [
+
+            Markup.button.callback(
+                "📩 Send Request",
+                "send_request"
+            )
+
+        ],
+
+        [
+
+            Markup.button.callback(
+                "❌ No Thanks",
+                "no_thanks"
+            )
+
+        ]
+
+    ]);
+
+}
+
+function adminButtons(userId) {
+
+    return Markup.inlineKeyboard([
+
+        [
+
+            Markup.button.callback(
+                "✅ Approve",
+                `approve_${userId}`
+            ),
+
+            Markup.button.callback(
+                "❌ Reject",
+                `reject_${userId}`
+            )
+
+        ]
+
+    ]);
+
+}
+
+function joinButton(link) {
+
+    return Markup.inlineKeyboard([
+
+        [
+
+            Markup.button.url(
+                "📢 Join Channel",
+                link
+            )
+
+        ]
+
+    ]);
+
+}
+
+module.exports = {
+
+    startButtons,
+
+    adminButtons,
+
+    joinButton
+
+};

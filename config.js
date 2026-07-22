bc110c11dd548d671f46993ceada88e2d9fb66ad
fxsharafuddin904-pixel <fxsharafuddin904@gmail.com>
2026-07-22 15:50:04 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,13 @@
-// config.js
+module.exports = {
+
+    BOT_TOKEN: "8775671267:AAHXKfG8KlxzHKmbmIZKWeOgq10IU1WM1uY",
+
+    ADMIN_ID: 8136997138,
+
+    CHANNEL_ID: -1004399158909,
+
+    CHANNEL_USERNAME: "",
+
+    BOT_NAME: "📚 Trading Learning Bot"
+
+};

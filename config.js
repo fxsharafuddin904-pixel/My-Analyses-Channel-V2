ace6442af3d909b8dd57a216798a974ee3b5cde0
fxsharafuddin904-pixel <fxsharafuddin904@gmail.com>
2026-07-25 11:29:46 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,32 @@
 module.exports = {
 
+    // Telegram Bot Token
     BOT_TOKEN: "8775671267:AAHXKfG8KlxzHKmbmIZKWeOgq10IU1WM1uY",
 
+    // Admin Telegram ID
     ADMIN_ID: 8136997138,
 
+    // Private Channel ID
     CHANNEL_ID: -1004399158909,
 
+    // Channel Username (Optional)
     CHANNEL_USERNAME: "",
 
+    // Bot Name
     BOT_NAME: "📚 Trading Learning Bot",
 
-    SUPPORT_USERNAME: "@MarketAnalysSupport"
+    // Support Username
+    SUPPORT_USERNAME: "@Smart_Method_Owner",
+
+    // Invite Link Settings
+    INVITE_EXPIRE_SECONDS: 3600, // 1 Hour
+
+    INVITE_MEMBER_LIMIT: 1,
+
+    // Welcome Message Settings
+    REQUEST_ENABLED: true,
+
+    // Notification
+    SEND_CHANNEL_NOTIFICATION: true
 
 };
